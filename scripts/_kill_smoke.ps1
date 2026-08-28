a4272bb3374bb56062ab0a6d612c7c3cb05e6ebd
Petr <sajner.petr@gmail.com>
2026-08-28 16:46:45 +1200

    Remove leftover one-off cleanup helper

--- scripts/_kill_smoke.ps1
+++
@@ -1,7 +0,0 @@
-Get-CimInstance Win32_Process |
-  Where-Object { $_.CommandLine -like '*qh-smoke*' } |
-  ForEach-Object {
-    Write-Output ("killing {0}: {1}" -f $_.ProcessId, $_.CommandLine.Substring(0, [Math]::Min(140, $_.CommandLine.Length)))
-    Stop-Process -Id $_.ProcessId -Force -ErrorAction SilentlyContinue
-  }
-Write-Output "---done---"
